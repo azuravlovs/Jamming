1e3d3bda781a3f01154dec9fed0fe4e10405e297
azuravlovs <75783039+azuravlovs@users.noreply.github.com>
2021-01-23 02:23:34 +0000

    Initial commit

---
+++ src/Components/SearchResult/SearchResult.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchResult.css';
+import TrackList from '../TrackList/TrackList';
+
+export class SearchResult extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults} />
+      </div>
+    )
+  }
+}
+
+export default SearchResult;
