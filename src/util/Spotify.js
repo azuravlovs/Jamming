cd2be23221f19a9a1bc5a111fb5547abb6672f61
azuravlovs <zuravlovsandris@gmail.com>
2021-01-24 08:48:51 +0200

    final build

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,9 @@
-const clientId = ''//SpotifyAPI here
-const redirectUri = 'http://localhost:3000'
+const clientId = 'e1eb898948724fc7a392c7113bd0ecfd'
+const redirectUri = 'http://azjamming.surge.sh'
 let accessToken = ''
 
 const Spotify = {
-    getAcessToekn() {
+    getAcessToken() {
         if(accessToken) {
             return accessToken;
         }
@@ -25,7 +25,7 @@ const Spotify = {
     },
 
     search(term) {
-        const accessToken = Spotify.getAcessToekn()
+        const accessToken = Spotify.getAcessToken()
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {Authorization: `Bearer ${accessToken}`}
         }).then(response => {
@@ -41,6 +41,36 @@ const Spotify = {
                 album: track.album.name,
                 uri: track.uri
             })))
+        })
+    },
+
+    savePlaylist(name, trackUris) {
+        if(!name || !trackUris.length) {
+            return
+        }
+        
+        const accessToken = Spotify.getAcessToken()
+        const headers = {Authorization: `Bearer ${accessToken}`}
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => response.json()
+        ).then(jsonResponse => {
+            userId = jsonResponse.id
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
+            {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: name})
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`,{
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({uris: trackUris})
+                })
+            })
         })
     }
 }
